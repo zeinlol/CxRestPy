d4219620db7ba8a089ec2c6f682f5503069c6a8c
zeinlol <nickolaiborshchov@gmail.com>
2022-04-14 20:14:48 +0300

    small changes for integration in another tools

--- etc/constants.py
+++ etc/constants.py
@@ -2,7 +2,7 @@ import pathlib
 
 REPORT_TYPES = ["PDF", "RTF", "CSV", "XML"]
 
-BASE_DIR = pathlib.Path().absolute()
+BASE_DIR = pathlib.Path(__file__).parents[1]
 
 SCAN_CHECK_SLEEP_TIME = 5  # in seconds
 REPORT_CHECK_SLEEP_TIME = 2  # in seconds
